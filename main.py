2b4a15e7b5f0c9e64ee60352294fc2d80d855901
megaraph <47447126+megaraph@users.noreply.github.com>
2023-02-11 21:34:44 +0800

    feat: add order exclusion feature

--- main.py
+++ main.py
@@ -35,6 +35,14 @@ DRINKS = [
     {"id": 4, "type": "Fruit Juice", "price": 65.0},
 ]
 
+MAPPING = {
+    1: "main",
+    2: "side",
+    3: "drink",
+}
+
+PROMPT_CHOICES = ["y", "n"]
+
 
 def main() -> None:
     print("\nWelcome! Would you like to order?")
@@ -42,24 +50,66 @@ def main() -> None:
 
     order_prompt = input("Answer [order/exit]: ").lower()
 
+    if order_prompt not in ["order", "exit"]:
+        print("Exiting... input not among the choices: 'order' or 'exit'")
+        return None
+
     if order_prompt == "exit":
         print("Exiting...")
         return None
 
-    if not order_prompt == "order":
-        print("Exiting... input not among the choices: 'order' or 'exit'")
-        return None
-
     num_members = int(input("\nHow many people are in your group: "))
 
+    orders = get_orders_from_user()
+
+    cancel_all = input("Cancel all orders (y/n)? ")
+
+    if cancel_all not in PROMPT_CHOICES:
+        # TODO: implement edge condition
+        pass
+
+    if cancel_all == "y":
+        print("Cancelled all orders...")
+        return None
+
+    exclude_item = input("Exclude an item from the total (y/n)? ")
+
+    if exclude_item not in PROMPT_CHOICES:
+        # TODO: implement edge condition
+        pass
+
+    if exclude_item == "y":
+        order_to_exclude = input("For which order? ")
+        # TODO: implement edge condition where order input does not belong or none at all
+
+        item_to_exclude = input("Which item will be excluded? ")
+        # TODO: implement edge condition where item input does not belong or none at all
+
+        for order in orders:
+            if order["order_num"] == int(order_to_exclude):
+                excluded_order = order
+                break
+
+        item = MAPPING[int(item_to_exclude)]
+        excluded_order[item]["price"] = 0
+        # TODO: add edge case to check if excluded order item is already empty
+        print(f"\n{excluded_order[item]['type']} will be excluded from the total.")
+
+    print(f"\nOrder for party of {num_members}")
+    checkout(orders, num_members)
+
+    return None
+
+
+def get_orders_from_user() -> list:
     orders = []
     order_num = 0
-    prompt_choices = ["y", "n"]
+
     while order_num < MAX_ORDERS:
         order = get_meal_order(order_num + 1)
-        order_is_correct = input("Is this meal set order correct (y/n):? ").lower()
+        order_is_correct = input("Is this order correct (y/n)? ").lower()
 
-        if order_is_correct not in prompt_choices:
+        if order_is_correct not in PROMPT_CHOICES:
             # TODO: implement edge condition
             pass
 
@@ -71,17 +121,14 @@ def main() -> None:
 
         next_order = input("Proceed with next order (y/n)? ").lower()
 
-        if next_order not in prompt_choices:
+        if next_order not in PROMPT_CHOICES:
             # TODO: implement edge condition
             pass
 
         if next_order == "n":
             break
 
-    print(f"\nOrder for party of {num_members}")
-    checkout(orders, num_members)
-
-    return None
+    return orders
 
 
 def get_meal_order(order_num: int):
@@ -97,19 +144,19 @@ def get_meal_order(order_num: int):
 
 
 def get_order_item(order_type: str, order_list: list) -> dict:
-    order_item_id = int(
-        input(
-            f"\t{order_type.title()}:\t\t",
-        )
+    order_item_id = input(f"\t{order_type.title()}:\t\t")
+
+    order_item = find_item_from_list(
+        order_list,
+        int(order_item_id) if len(order_item_id) > 0 else 0,
     )
-    order_item = find_item_from_list(order_list, order_item_id)
     print(f"\t\t{order_item.get('type')}")
 
     return order_item
 
 
 def find_item_from_list(order_list: list, order_id: int) -> dict:
-    if order_id == None:
+    if order_id == 0 or order_id is None:
         return {}
 
     for item in order_list:
